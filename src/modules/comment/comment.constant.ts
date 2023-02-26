ccaee02ecd7b5e18cef7ed0c8205d6dd56304c9f
rklebede <RLebedev@stream.ru>
2023-02-26 19:36:31 +0700

    fix: add sort and limit for comments

--- src/modules/comment/comment.constant.ts
+++ src/modules/comment/comment.constant.ts
@@ -7,3 +7,5 @@ export enum RatingRange {
     min = 1,
     max = 5
 }
+
+export const MAX_COMMENT_COUNT = 50;
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -4,6 +4,8 @@ import { CommentServiceInterface } from './comment-service.interface.js';
 import { Component } from '../../types/component.types.js';
 import { CommentEntity } from './comment.entity.js';
 import CreateCommentDto from './dto/create-comment.dto.js';
+import { SortType } from '../../types/sort-type.enum.js';
+import { MAX_COMMENT_COUNT } from './comment.constant.js';
 
 @injectable()
 export default class CommentService implements CommentServiceInterface {
@@ -18,8 +20,7 @@ export default class CommentService implements CommentServiceInterface {
 
   public async findByOfferId(offerId: string): Promise<DocumentType<CommentEntity>[]> {
     return this.commentModel
-      .find({offerId})
-      .populate('userId');
+      .find({offerId}).sort({createdAt: SortType.Down}).limit(MAX_COMMENT_COUNT).populate('userId');
   }
 
   public async deleteByOfferId(offerId: string): Promise<number> {
